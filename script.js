0d0f039162debd49e4820e10ef4a46d912d3781e
Nolan Stone <88562716+3xionDev@users.noreply.github.com>
2025-03-06 11:33:57 -0500

    change custom url so subdomaining works

---
+++ script.js
@@ -0,0 +1,71 @@
+function ngu() {
+  window.open("https://3xiondev.github.io/n-gon-upgraded/", "_self");
+}
+
+function collatz() {
+  window.open("https://3xiondev.github.io/collatz/", "_self");
+}
+
+function ndocs() {
+  window.open("https://github.com/3xiondev/n-docs/", "_self");
+}
+
+function format() {
+  window.open("https://github.com/3xiondev/formattingcode/", "_self");
+}
+
+function clicker() {
+  window.open("https://github.com/3xiondev/hack.halloween/", "_self");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const cursor = document.createElement("div");
+  cursor.className = "cursor";
+  document.body.appendChild(cursor);
+
+  document.addEventListener("mousemove", function(e) {
+    const cursorX = e.clientX;
+    const cursorY = e.clientY;
+
+    cursor.style.left = cursorX + "px";
+    cursor.style.top = cursorY + "px";
+    })
+  document.addEventListener("mousedown", function() {
+    cursor.style.width = 10 + "px";
+    cursor.style.height = 10 + "px";
+  })
+  document.addEventListener("mouseup", function() {
+    cursor.style.width = 15 + "px";
+    cursor.style.height = 15 + "px";
+  })
+
+  const cursor2 = document.createElement("div");
+  cursor2.className = "cursor2";
+  document.body.appendChild(cursor2);
+
+  document.addEventListener("mousemove", function(e) {
+    const cursor2X = e.clientX;
+    const cursor2Y = e.clientY;
+
+    cursor2.style.left = cursor2X + "px";
+    cursor2.style.top = cursor2Y + "px";
+    })
+  document.addEventListener("mousedown", function() {
+    cursor2.style.width = 40 + "px";
+    cursor2.style.height = 40 + "px";
+  })
+  document.addEventListener("mouseup", function() {
+    cursor2.style.width = 15 + "px";
+    cursor2.style.height = 15 + "px";
+  })
+  })
+
+if (navigator.userAgent.match(/Android/i)
+         || navigator.userAgent.match(/webOS/i)
+         || navigator.userAgent.match(/iPhone/i)
+         || navigator.userAgent.match(/iPad/i)
+         || navigator.userAgent.match(/iPod/i)
+         || navigator.userAgent.match(/BlackBerry/i)
+         || navigator.userAgent.match(/Windows Phone/i)) {
+          window.open("mobile.html", "_self");
+}
